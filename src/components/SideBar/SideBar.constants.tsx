f4776cdb59c24930df6a73edb0c5c9ccb47d4e20
ilya <ilyaeruh@gmail.com>
2022-12-25 14:35:55 +0200

    path fix

--- src/components/SideBar/SideBar.constants.tsx
+++ src/components/SideBar/SideBar.constants.tsx
@@ -12,11 +12,11 @@ import { RiTeamFill } from 'react-icons/ri';
 import { SiFiles } from 'react-icons/si';
 
 export const navLinks = [
-  { id: 1, to: '/', text: 'Home', icon: <FaHome /> },
-  { id: 2, to: '/', text: 'Team', icon: <RiTeamFill /> },
-  { id: 3, to: '/', text: 'Projects', icon: <FaFolderOpen /> },
-  { id: 4, to: '/', text: 'Calendar', icon: <FaCalendarAlt /> },
-  { id: 5, to: '/', text: 'Documents', icon: <SiFiles /> },
+  { id: 1, to: '', text: 'Home', icon: <FaHome /> },
+  { id: 2, to: '', text: 'Team', icon: <RiTeamFill /> },
+  { id: 3, to: '', text: 'Projects', icon: <FaFolderOpen /> },
+  { id: 4, to: '', text: 'Calendar', icon: <FaCalendarAlt /> },
+  { id: 5, to: '', text: 'Documents', icon: <SiFiles /> },
 ];
 
 export const footerLinks = [
