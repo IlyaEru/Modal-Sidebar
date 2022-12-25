30d501c7e44aab657c5f383da25c510972c94286
ilya <ilyaeruh@gmail.com>
2022-12-25 14:30:12 +0200

    github pages path error fix

--- src/components/SideBar/SideBar.constants.tsx
+++ src/components/SideBar/SideBar.constants.tsx
@@ -13,10 +13,10 @@ import { SiFiles } from 'react-icons/si';
 
 export const navLinks = [
   { id: 1, to: '/', text: 'Home', icon: <FaHome /> },
-  { id: 2, to: '/team', text: 'Team', icon: <RiTeamFill /> },
-  { id: 3, to: '/projects', text: 'Projects', icon: <FaFolderOpen /> },
-  { id: 4, to: '/calendar', text: 'Calendar', icon: <FaCalendarAlt /> },
-  { id: 5, to: '/documents', text: 'Documents', icon: <SiFiles /> },
+  { id: 2, to: '/', text: 'Team', icon: <RiTeamFill /> },
+  { id: 3, to: '/', text: 'Projects', icon: <FaFolderOpen /> },
+  { id: 4, to: '/', text: 'Calendar', icon: <FaCalendarAlt /> },
+  { id: 5, to: '/', text: 'Documents', icon: <SiFiles /> },
 ];
 
 export const footerLinks = [
